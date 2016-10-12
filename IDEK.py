ab3422d711f9394c53ad1bb5b64f09033275aa41
KyleCow <9131@kingdown.wilts.sch.uk>
2016-10-12 17:05:34 +0100

    Create IDEK.py

---
+++ IDEK.py
@@ -0,0 +1,39 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~(C) KyleCow 2016~~~~~~~~#
+#~There Are No Legal Documents~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+import time
+print ("You wake up unaware of whats going on")
+time.sleep(1)
+print ("You untie the noose around your neck")
+time.sleep(1)
+print ("Opening your eyes you see keemdaddy")
+time.sleep(1)
+print ("He Yells in your face")
+time.sleep(1)
+print ("DRAMAALERT NATION NOW OVER 1 SUBSCRIBEEEER")
+time.sleep(0.3)
+print ("You Punch his gnome like face")
+time.sleep(0.1)
+print ("He bleeds orange juice")
+time.sleep(1)
+print ("While he's dying you drink his orange juice")
+time.sleep(1)
+name=input("What is your name? ")
+print ("SUP "+name)
+time.sleep(2)
+command=input("Are you aware of the mythical the scarce? ")
+if command == "n" or "N" or "No" or "NO" or "no":
+    print("Once apon a time")
+    time.sleep(2)
+    print("a youtube channel")
+    time.sleep(1)
+    print("called scarce")
+    time.sleep(2)
+    print("was created")
+    time.sleep(1)
+    print("You Kill Yourself Out Of Bordom")
+    time.sleep(1)
+    quit()
+else:
+    print("Cool so you already know well thats great and all, but i really wanted to explain it to you but fiiine just leave")
