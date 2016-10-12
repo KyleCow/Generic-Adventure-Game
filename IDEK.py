0ff33fc4b4c17cb3b309b8161bf24ca24335c1aa
KyleCow <9131@kingdown.wilts.sch.uk>
2016-10-12 19:31:56 +0100

    Update IDEK.py

--- IDEK.py
+++ IDEK.py
@@ -1,6 +1,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~(C) KyleCow 2016~~~~~~~~#
 #~There Are No Legal Documents~#
+#~~~To Back This Statement Up~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 import time
 print ("You wake up unaware of whats going on")
@@ -19,16 +20,16 @@ print ("He bleeds orange juice")
 time.sleep(1)
 print ("While he's dying you drink his orange juice")
 time.sleep(1)
-name=input("What is your name? ")
+name=input("Keem: What is your name? ")
 print ("SUP "+name)
 time.sleep(2)
 command=input("Are you aware of the mythical scarce? ")
 if command == "n" or command == "N" or command == "No" or command == "NO" or command == "no":
-    print("Once apon a time")
+    print("Keem: Once apon a time")
     time.sleep(2)
     print("a youtube channel")
     time.sleep(1)
-    print("called scarce")
+    print("called Scarce")
     time.sleep(2)
     print("was created")
     time.sleep(1)
@@ -36,10 +37,28 @@ if command == "n" or command == "N" or command == "No" or command == "NO" or com
     time.sleep(1)
     quit()
 else:
-    print("Cool so you already know well thats great and all, but i really wanted to explain it to you but fiiine just leave")
+    print("Keem: Cool so you already know well thats great and all, but i really wanted to explain it to you but fiiine just leave")
 leave=input("Press L to leave ")
 if leave == "L" or leave == "l":
     print("You Leave")
 else:
     print("You Die Alone")
     quit()
+time.sleep(1)
+print("The sun blinds you")
+time.sleep(1)
+print("Keem: Hello, Im in your head now")
+time.sleep(0.6)
+print("LOL")
+print("Where do you want to go to?")
+Location=input("Type K for kingdom, Type H for hills or Type V for Village")
+if Location == "K" or Location == "k":
+    print("Welcome To The Kingdom Where None Of Your Dreams Will Come True")
+else:
+    if Location == "H" or Location == "h":
+        print("Lol u r alone")
+    else:
+        if Location == "V" or Location == "v":
+            print("Welcome to the village there is nothing here")
+        else:
+            print("Fuck OFF")
